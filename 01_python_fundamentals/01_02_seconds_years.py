ab81884e7e9374c76e53a2764c13a5e42a67e3fe
caveman0612 <75807458+caveman0612@users.noreply.github.com>
2021-01-30 11:06:56 -0800

    first three labs of section one

--- 01_python_fundamentals/01_02_seconds_years.py
+++ 01_python_fundamentals/01_02_seconds_years.py
@@ -4,3 +4,11 @@ Move the code you previously wrote to calculate how many seconds are in a year i
 Then execute it as a script to see the output printed to your console.
 
 '''
+
+seconds = 60
+minutes = 60
+hours = 24
+days = 365
+
+total = seconds*minutes*hours*days
+print(total)
