9b160fcd73f304f55a01695a43f616e38818e0f5
caveman0612 <75807458+caveman0612@users.noreply.github.com>
2021-01-30 12:05:57 -0800

    finished rest of the first module

--- 01_python_fundamentals/01_04_formula.py
+++ 01_python_fundamentals/01_04_formula.py
@@ -4,3 +4,8 @@ Write the necessary code to print the result of the following formula:
 	(15.7 * 3.6 - 34.9 * 0.9) / (68.9 - 2.1)
 
 '''
+
+unit1 = (15.7 * 3.6 - 34.9 * 0.9)
+unit2 = (68.9 - 2.1)
+
+print(unit1/unit2)
