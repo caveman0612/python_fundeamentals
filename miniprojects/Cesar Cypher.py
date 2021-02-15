47eb68d4d7ad1e5ff0a03c8a3ee59493259e49ce
caveman0612 <75807458+caveman0612@users.noreply.github.com>
2021-02-15 11:02:20 -0800

    finished Cesar Cypher mini project

---
+++ miniprojects/Cesar Cypher.py
@@ -0,0 +1,42 @@
+
+def encode_Cesar_cypher(string, cipher=7):
+    coded_list = []
+    string = string.replace(" ", "")
+    string = string.lower()
+    for letter in string:
+        num = ord(letter)
+        num = num + 7
+        if num > 122:
+            inverse = num - 122
+            new_num = 96 + inverse
+            char = chr(new_num)
+            coded_list.append(char)
+        else:
+            char = chr(num)
+            coded_list.append(char)
+    string = "".join(coded_list)
+    return string
+
+def decode_Cesar_cypher(string, cipher=7):
+    decoded_list = []
+    for letter in string:
+        num = ord(letter)
+        num = num - 7
+        if num < 97:
+            inverse = 97 - num
+            new_num = 123 - inverse
+            char = chr(new_num)
+            decoded_list.append(char)
+        else:
+            char = chr(num)
+            decoded_list.append(char)
+    string = "".join(decoded_list)
+    return string
+
+
+
+
+code = encode_Cesar_cypher("hello zanthor I hope you are having a great day")
+print(code)
+decode = decode_Cesar_cypher(code)
+print(decode)
