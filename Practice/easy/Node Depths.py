3b929903586f13bdaffd7f382b802bb602ac647e
caveman0612 <kyle.stinar@hotmail.com>
2021-03-15 09:36:07 -0700

    got 80%done

--- Practice/easy/Node Depths.py
+++ Practice/easy/Node Depths.py
@@ -1,6 +1,23 @@
 def nodeDepths(root):
-    # Write your code here.
-    pass
+    global total
+    total = 0
+    nodeDepthHelper(root)
+    print(total)
+    return total
+
+def nodeDepthHelper(root, d=0):
+    depth = d + 1
+    if root.left == None and root.right == None:
+        global total
+        total += depth
+    if root.left == None and root.right != None:
+        nodeDepthHelper(root.left, depth)
+    if root.left != None and root.right == None:
+        nodeDepthHelper(root.right, depth)
+    if root.left != None and root.right != None:
+        nodeDepthHelper(root.left, depth)
+        nodeDepthHelper(root.right, depth)
+
 
 
 # This is the class of the input binary tree.
@@ -9,3 +26,17 @@ class BinaryTree:
         self.value = value
         self.left = None
         self.right = None
+
+tree = BinaryTree(1)
+tree.left = BinaryTree(2)
+tree.right = BinaryTree(3)
+tree.left.left = BinaryTree(4)
+tree.left.right = BinaryTree(5)
+tree.left.left.left = BinaryTree(8)
+tree.left.left.right = BinaryTree(9)
+tree.right.left = BinaryTree(6)
+tree.right.right = BinaryTree(7)
+
+nodeDepths(tree)
+
+
