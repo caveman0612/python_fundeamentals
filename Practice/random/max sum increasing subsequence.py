5403f380140fbcb3dce205a9fda9c4cf935e92ee
caveman0612 <kyle.stinar@hotmail.com>
2021-04-10 10:00:27 -0700

    algo problem not done

--- Practice/random/max sum increasing subsequence.py
+++ Practice/random/max sum increasing subsequence.py
@@ -1,34 +1,19 @@
 def maxSumIncreasingSubsequence(array):
-    answer = [float("-inf")]
-    for idx in range(len(array)):
-        temp = compare_with_lower(array[:idx+1])
-        if temp[0] > answer[0]:
-            answer = temp
-    return answer
-
-def compare_with_lower(array):
-    reverse_arr = array[::-1]
-    current_lowerst = float("inf")
-    total = float('-inf')
-    lst = []
-    for num in reverse_arr:
-        if num < current_lowerst:
-            temp = total
-            temp += num
-            if temp >= total:
-                lst.append(num)
-                if total == float("-inf"):
-                    total = num
-                    current_lowerst = num
-                else:
-                    total += num
-                    current_lowerst = num
-    return [total, lst[::-1]]
+    sums = [num for num in array]
+    prev_lists = [None for num in array]
+    print(sums, prev_lists)
+    for i in range(len(array)):
+        current_num = array[i]
+        for j in range(0, i):
+            prev_num = array[j]
+            if current_num > prev_num and current_num + sums[j] > sums[i]:
+                sums[i] = current_num + sums[j]
 
 
 
 
-# dict1 = {0:10, 1:70, 2:20, 3:30, 4:50, 5:11, 6:30}
+
+
 # array = [10,70,20,30,50,11,30]
 # array = [-5, -4, -3, -2, -1]
 # array = [-1, 1]
@@ -38,5 +23,4 @@ array = [8, 12, 2, 3, 15, 5, 7]
 # print(compare_with_lower(dict1, 2))
 
 print(maxSumIncreasingSubsequence(array))
-#answer [110, [10,20,30,50]]
 
